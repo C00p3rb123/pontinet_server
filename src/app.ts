33f638b50fc7208273506bfc586296db7cdc81c6
C00p3rb123 <cooperbennison20@gmail.com>
2023-10-17 15:34:45 +1000

    init whatsappv2

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import swaggerjsdoc from "swagger-jsdoc";
 import swaggerUI = require("swagger-ui-express");
 import registrationRouter from "../src/routes/registration"
+import messagesRouter from "../src/routes/whatsapp"
 const app = express();
 dotenv.config();
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT;
 
 app.use(morgan("tiny"));
 app.use("/registration", registrationRouter);
+app.use("/whatsapp", messagesRouter);
 
 app.get("/hello/testing", (req, res) => {
   console.log("Hello xcvxcv");
