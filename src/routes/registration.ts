5b48df48e74fcf681753ae4318cbd3eacb0b3143
n10507621 <129021402+n10507621@users.noreply.github.com>
2023-10-18 11:38:52 +1000

    Feat: Clinic Details Endpoint

--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -1,15 +1,22 @@
-import express from "express"
+import express, { Router, Request, Response } from "express";
 import dotenv from "dotenv"
 import axios from "axios"
 
 
-const router = express.Router();
+const router: Router = express.Router();
+router.use(express.json());
+router.post("/clinic-details", (req: Request, res: Response) => {
+   
+    const { clinicName, clinicCountry, clinicSuburb, clinicOnCallNumber } = req.body;
 
+   
+    if (!clinicName || !clinicCountry || !clinicSuburb || !clinicOnCallNumber) {
+        return res.status(400).json({ error: "Invalid clinic details" });
+    }
 
-router.get("/clinic-details", (req, res) => {
-    
-    console.log("Registration")
-})
+  
+    return res.status(200).json({ message: "Clinic details received successfully" });
+});
 
-export default router
+export default router;
 
