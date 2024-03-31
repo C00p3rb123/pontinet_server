3bcd8d596a86b453f4b850351bb23df7e21bd509
C00p3rb123 <cooperbennison20@gmail.com>
2024-03-31 10:35:35 +1000

    auth bug fix

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -26,7 +26,7 @@ export const verifyToken = (req: any, res: any, next: any) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader.split(" ")[1];
   if(!token){
-    return res.status(401).json({ message: 'Access denied. No token provided.' });
+    return res.status(403).json({ message: 'Access denied. No token provided.' });
   }
   try{
     const decoded = jwt.verify(token, process.env.SECRET!);
